6b1568cc5a1835d121b939e131dc999ce4f14707
David Gaid <david.gayed1997@gmail.com>
2023-10-05 15:34:04 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C Program That prints with pust functions
+ * Return always 0 (success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
