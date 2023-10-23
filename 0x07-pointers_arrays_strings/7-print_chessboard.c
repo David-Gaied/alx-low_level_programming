860f5f00c0aaee1a758b57d3c0c2d97b6bdc5a00
David Gaid <david.gayed1997@gmail.com>
2023-10-23 16:18:29 -0700

    variables

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,37 +1,21 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 /**
- * cap_string - capitalizes most of the words in a string.
- * @s: analized string.
+ * print_chessboard - print a chessboard.
+ * @a: source matrix
  *
- * Return: String with all words capitalized.
+ *
+ * Return: chessboard.
  */
-char *cap_string(char *s)
+void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	i = 0;
-	while (*(s + i) != '\0')
+	for (i = 0; i <= 7; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		for (j = 0; j <= 7; j++)
 		{
-			if (i == 0)
-			{
-				*(s + i) = *(s + i) - 32;
-			}
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-					{
-						*(s + i) = *(s + i) - 32;
-					}
-				}
-			}
+			_putchar(a[i][j]);
 		}
-	i++;
+		_putchar('\n');
 	}
-	return (s);
-}
+}}
