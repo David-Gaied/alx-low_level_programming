27b1984361b2b39e48123816eaf63a8db8af116c
David Gaid <david.gayed1997@gmail.com>
2023-11-02 16:34:06 -0700

    variables

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,87 +1,99 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * _puts - prints a string followed by a new newline
- * @str: str to print
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-
-void _puts(char *str)
+int is_digit(char *s)
 {
-	int a = 0;
+	int i = 0;
 
-	while (str[a])
+	while (s[i])
 	{
-		_putchar(str[a]);
-		a++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
 }
 
 /**
- * _atoi - converts a string to an int
- * @s: pointer to string
- * Return: converted int
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-
-int _atoi(const char *s)
+int _strlen(char *s)
 {
-	int sign = 1;
-	unsigned long int resp = 0, first, a;
+	int i = 0;
 
-	for (first = 0; !(s[first] >= 48 && s[first] <= 57); first++)
-		if (s[first] == '-')
-			sign *= -1;
-
-	for (a = first; s[a] >= 48 && s[a] <= 57; a++)
+	while (s[i] != '\0')
 	{
-		resp *= 10;
-		resp += (s[a] - 48);
+		i++;
 	}
-
-	return (sign * resp);
+	return (i);
 }
 
 /**
- * print_int - prints an integer
- * @n: int
- * Return: void
+ * errors - handles errors for main
  */
-
-void print_int(unsigned long int n)
+void errors(void)
 {
-	unsigned long int divisor = 1;
-	unsigned long int a, resp;
-
-	for (a = 0; n / divisor > 9; a++, divisor *= 10)
-		;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * main - returns the product of two positive numbers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
- * @argv: arguments
- * Return: 0
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3 || !_atoi(argv[1]) || !_atoi(argv[2]))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		_puts("Error\n");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-
+	free(result);
 	return (0);
 }
