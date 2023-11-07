0dd69b935bb4d6a3a7df979dec1b4fefe64977dc
David Gaid <david.gayed1997@gmail.com>
2023-11-06 17:15:15 -0800

    variables

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN__H
+#define MAIN_H_
+#include <stdio.h>
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
