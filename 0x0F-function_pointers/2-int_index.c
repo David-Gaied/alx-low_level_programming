4f07482289d3b367f9eace8e7bf3cc34e527d824
David Gaid <david.gayed1997@gmail.com>
2023-11-09 16:00:07 -0800

    variables

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,17 +1,32 @@
+/*
+ * File: 2-int_index.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "function_pointers.h"
 
 /**
- * array_iterator - Execute a function over array elements.
- * @array: array to iterate.
- * @size: array size.
- * @action: function to execute.
+ * int_index - Searches for an integer in an array of integers.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @cmp: A pointer to the function to be used to compare values.
  *
+ * Return: If no element matches or size <= 0 - -1.
+ *         Otherwise - The index of the first element for which
+ *                     the cmp function does not return 0.
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int index;
 
-	if (array && action)
-		for (i = 0; i < (int)size; i++)
-			action(array[i]);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
 }
